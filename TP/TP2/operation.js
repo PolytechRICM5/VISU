c017a301a835f10c8bbd8d2d23b766ad232c9102
Alicia Abonnenc <alicia.abonnenc@hotmail.com>
2017-10-10 15:07:52 +0200

    etapeDecomposition

--- TP/TP2/operation.js
+++ TP/TP2/operation.js
@@ -15,5 +15,43 @@ data = [];
 data[0] = [0,0];
 data[1] = [20,30];
 data[2] = [30,50];
+data[3] = [30,60];
 
 draw(data);
+
+
+function EtapeDecomposition(data,taille){
+	var x = [];
+	var y = [];
+
+	x[0] =[
+		( - data[taille-2][0] + 3 * data[taille-1][0] + 3 * data[0][0] - data[1][0]) / 4,
+		( - data[taille-2][1] + 3 * data[taille-1][1] + 3 * data[0][1] - data[1][1]) / 4
+	];
+	y[0] = [
+		( data[taille-2][0] - 3 * data[taille-1][0] + 3 * data[0][0] - data[1][0]) / 4,
+		( data[taille-2][1] - 3 * data[taille-1][1] + 3 * data[0][1] - data[1][1]) / 4
+	];
+
+	for( var i = 1; i < taille/2; i++)
+	{
+		x[i] = [
+			( - data[2*i-2][0] + 3 * data[(2*i)-1][0] + 3 * data[2*i][0] - data[2*i+1][0]) / 4,
+			( - data[2*i-2][1] + 3 * data[(2*i)-1][1] + 3 * data[2*i][1] - data[2*i+1][1]) / 4
+		];
+		y[i] = [
+			( data[2*i-2][0] - 3 * data[(2*i)-1][0] + 3 * data[2*i][0] - data[2*i+1][0]) / 4,
+			( data[2*i-2][1] - 3 * data[(2*i)-1][1] + 3 * data[2*i][1] - data[2*i+1][1]) / 4
+		];
+	}
+/*
+	x[max][0] = ( - data[2*i-2][0] + 3 * data[(2*i)-1][0] + 3 * data[2*i][0] - data[2*i+1][0]) / 4;
+	x[max][1] = ( - data[2*i-2][1] + 3 * data[(2*i)-1][1] + 3 * data[2*i][1] - data[2*i+1][1]) / 4;
+	y[max][0] = ( data[2*i-2][0] - 3 * data[(2*i)-1][0] + 3 * data[2*i][0] - data[2*i+1][0]) / 4;
+	y[max][1] = ( data[2*i-2][1] - 3 * data[(2*i)-1][1] + 3 * data[2*i][1] - data[2*i+1][1]) / 4;
+*/
+	return x.concat(y);
+}
+
+console.log(EtapeDecomposition(data,4));
+console.log(data[0][0] - data[1][0]);
