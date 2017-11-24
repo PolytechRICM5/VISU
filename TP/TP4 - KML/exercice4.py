1e154fc4e756334b99d909037bc457c270bd4217
Gilles B <6406.number@gmail.com>
2017-11-24 14:58:09 +0100

    ex 3 and 4

--- TP/TP4 - KML/exercice4.py
+++ TP/TP4 - KML/exercice4.py
@@ -2,6 +2,8 @@
 
 file = open("data-tp2/cities.txt", "r");
 
+year = 2000
+
 print (
 '<?xml version="1.0" encoding="UTF-8"?>\n'
 '<kml xmlns="http://www.opengis.net/kml/2.2">\n'
@@ -14,11 +16,14 @@ for line in file :
     print(
         '<Placemark>\n'
         '<name>' + word[0] + ' : ' + word[3] + 'M hab.</name>\n'
-        '<Point>\n'
+        '<TimeSpan>\n'
+        '<begin>' + str(year) + '</begin>\n'
+        '</TimeSpan>\n''<Point>\n'
         '<coordinates>' + word[1] + ',' + word[2] + '</coordinates>\n'
         '</Point>\n'
         '</Placemark>\n'
     )
+    year+=1
 
 print(
 '</Document>\n'
